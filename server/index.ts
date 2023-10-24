0fbec0378625a7186d116e93644fecd539e71746
Reka <reka.reka.rn@gmail.com>
2023-10-24 14:19:22 +0700

    add CORS setup

--- server/index.ts
+++ server/index.ts
@@ -10,6 +10,12 @@ const FE_URI = process.env.REACT_APP_URI ?? 'http://localhost:3000';
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-API-KEY");
+  next();
+});
+
 // This server reuses the serverless endpoints from the "plugin-rtc" Twilio CLI Plugin, which is used when the "npm run deploy:twilio-cli" command is run.
 // The documentation for this endpoint can be found in the README file here: https://github.com/twilio-labs/plugin-rtc
 const tokenFunction: ServerlessFunction = require('@twilio-labs/plugin-rtc/src/serverless/functions/token').handler;
